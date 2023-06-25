6b10d9ff89383a81765abce10e8d8ae7f85cf084
Fabiano Teodoro <faateodoro@gmail.com>
2023-06-25 12:44:57 -0300

    Iniciando API de Posts

---
+++ Blog/Controllers/PostController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PostController : ControllerBase
+    {
+        [HttpGet("/")]
+        public IActionResult index()
+        {
+            return Ok("O Controller funcionou corretamente");
+        }
+    }
+}
---
+++ Blog/Entities/Post.cs
@@ -0,0 +1,11 @@
+﻿namespace Blog.Entities
+{
+    public class Post
+    {
+        public uint Id { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+
+    }
+}
