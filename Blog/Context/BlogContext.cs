1e0d28a34c9af21e7d5be4c8749a929ddc675295
Fabiano Teodoro <faateodoro@gmail.com>
2023-06-25 13:00:29 -0300

    Conectando com o banco de dados

---
+++ Blog/Context/BlogContext.cs
@@ -0,0 +1,15 @@
+﻿using Blog.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Context
+{
+    public class BlogContext : DbContext
+    {
+        DbSet<Post> Posts { get; set; }
+
+        public BlogContext(DbContextOptions<BlogContext> options) : base(options)
+        {
+            
+        }
+    }
+}
--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,6 +1,12 @@
+using Blog.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<BlogContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
